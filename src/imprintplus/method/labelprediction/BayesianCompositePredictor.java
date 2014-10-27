a30abd093469bb21704b7588825967f19c3d5c9f
SHAREEF <SHAREEF@ABED-SHAREEF>
2014-10-27 17:01:29 +0100

    bayesianCompositePredictor

---
+++ src/imprintplus/method/labelprediction/BayesianCompositePredictor.java
@@ -0,0 +1,25 @@
+package imprintplus.method.labelprediction;
+
+import imprintplus.data.LongitudinalObject;
+import imprintplus.data.LongitudinalTable;
+
+/**
+ * 
+ * @author SHAREEF
+ *
+ */
+public class BayesianCompositePredictor extends AbstractComposite {
+
+	@Override
+	public String predict(LongitudinalObject _obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void learn(LongitudinalTable _table) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
