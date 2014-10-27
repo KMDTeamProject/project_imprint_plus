93540bd4b86309aaab132d86005f0faa2ca13b3a
siddiqui16 <zaigham16@gmail.com>
2014-10-27 13:15:37 +0100

    unit_test

---
+++ src/imprintplus/unit_test/UnitTest_LongitudinalObject.java
@@ -0,0 +1,24 @@
+package imprintplus.unit_test;
+
+import junit.framework.TestCase;
+
+/**
+ * Junit Test for the LongitudinalObject.
+ * 
+ * TODO: Implement this class before implementing
+ * the functionalities in LongitudinalObject.
+ * 
+ * @author siddiqui
+ */
+public class UnitTest_LongitudinalObject extends TestCase {
+
+	@Override
+	protected void setUp() {
+		// TODO
+	}
+	
+	@Override
+	protected void tearDown() {
+		// TODO
+	}
+}
