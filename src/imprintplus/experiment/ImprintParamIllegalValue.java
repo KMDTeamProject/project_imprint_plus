2292923ede0f546e020735ea61bb7c9d24cac04b
siddiqui16 <zaigham16@gmail.com>
2014-11-04 18:00:58 +0100

    Exception Handling

--- src/imprintplus/experiment/ImprintParamIllegalValue.java
+++ src/imprintplus/experiment/ImprintParamIllegalValue.java
@@ -5,6 +5,7 @@ package imprintplus.experiment;
  * @author siddiqui16
  */
 public class ImprintParamIllegalValue extends ImprintExpException {
+	
 	private static final long serialVersionUID = 1L;
 
 	public ImprintParamIllegalValue(String _msg) {
