36a86bb853f8befe4f87f4232bd21b6dec9f640f
siddiqui16 <zaigham16@gmail.com>
2014-11-06 17:59:35 +0100

    JUnit Test Suite

---
+++ src/imprintplus/unit_test/AllTests.java
@@ -0,0 +1,11 @@
+package imprintplus.unit_test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ JUnit_LongitudinalTable_Test1.class, JUnit_Parameters.class })
+public class AllTests {
+
+}
