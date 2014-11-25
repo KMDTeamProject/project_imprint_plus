f6d3a974f26b2ca0611aa788b403a4312a642eba
SHAREEF <SHAREEF@ABED-SHAREEF.fritz.box>
2014-11-25 02:38:09 +0100

    update the nGram classes

--- src/imprintplus/method/labelprediction/UnitaryNGram.java
+++ src/imprintplus/method/labelprediction/UnitaryNGram.java
@@ -2,7 +2,12 @@ package imprintplus.method.labelprediction;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import com.sun.corba.se.impl.oa.poa.ActiveObjectMap.Key;
 
 import imprintplus.data.LongitudinalObject;
 
@@ -12,54 +17,76 @@ import imprintplus.data.LongitudinalObject;
 
 
 public class UnitaryNGram extends AbstractUnitary {
+	
+	private int n = 3;
+	//nGram variable that will contain the labels along with the 
+    private Hashtable<String, Hashtable<String, Double>> nGram = new Hashtable<String, Hashtable<String, Double>>();
+    
 	@Override
+	/**
+	 * Unitary predictor doesn't learn a model. When a LongitudinalObject
+	 * is provided to them, they learn the model on the fly and do the 
+	 * prediction. The learning is also encapsulated inside this method. 
+	 */
 	public String predict(LongitudinalObject _obj)
 			throws ImprintMethodMissingDataException {
 		
+
 		/* Get the label series of the longitudinal object _obj. */
 		ArrayList<String> l_series = _obj.getLabelSeries();
-		
-		/*
-		 * Get the last index and then get the last label
-		 */		
-		int last_label_index = l_series.size()-1;
-		String last_label = null;
-		last_label =  l_series.get(last_label_index);
-		
-		
-		/* Count the occurrence of each label in the label series
-		 * that goes with last label */
-		Map<String, Integer> label_counts = new HashMap<String, Integer>();
-		for (int counter = 0 ; counter < l_series.size()-1; counter++){
-			
-			if (l_series.get(counter).equals(last_label))
-			{		
-				String l = l_series.get(counter+1);
-				if (!label_counts.containsKey(l))
-					label_counts.put(l, 0);
-				label_counts.put(l, label_counts.get(l) + 1);				
-			}
-		}
-			
-		for (String l : l_series) {
-			if (!label_counts.containsKey(l))
-				label_counts.put(l, 0);
-			label_counts.put(l, label_counts.get(l) + 1);
-		}
-		/*
-		 * Search for the label with the maximum counts while label is 
-		 * same as last label and then return the max
-		 * label as the predicted label.
-		 */
-		String max_label = null;
-		int max_count = Integer.MIN_VALUE;
 
-		for (String l : label_counts.keySet()) {
-			if (label_counts.get(l) > max_count) {
-				max_label = l;
-				max_count = label_counts.get(l);
-			}
-		}
-		return max_label;
+		//building the n-gram for the label series with n = 3 
+		for(int i = 0; i <= l_series.size()-n; i++) {
+            if(nGram.containsKey(l_series.get(i) + " " + l_series.get(i+1))) {
+
+                if(nGram.get(l_series.get(i)+" "+l_series.get(i+1)).containsKey(l_series.get(i+2))) 
+                {
+                    double v = nGram.get(l_series.get(i) + " " + l_series.get(i+1)).get(l_series.get(i+2));
+                    v++;
+                    nGram.get(l_series.get(i) + " " + l_series.get(i+1)).put(l_series.get(i+2), v);
+                } else {
+                    nGram.get(l_series.get(i) + " " + l_series.get(i+1)).put( l_series.get(i+2), 1.0);
+                }
+            } else {
+
+                nGram.put(l_series.get(i)+" "+l_series.get(i+1), create(l_series.get(i+2)));
+            }
+        }
+		
+		
+		/*
+		 * Get the index and then the last n-label
+		 */		
+		int last_label_index = l_series.size()-n-1;
+		String last_label = "";
+		
+		last_label =  l_series.get(last_label_index)+" "+l_series.get(last_label_index+1);
+		
+		/*
+		 * Search the nGram for the label with the best confidence while current label is 
+		 * same as last n-label 
+		 */
+		
+		String predicate_label = null;
+		double bestConfidence = Double.MIN_VALUE;
+
+		for(Entry<String, Double> entry : nGram.get(last_label).entrySet()) {
+	        String currentPrediction = entry.getKey();
+	        double currentConfidence = entry.getValue();
+
+	        if (currentConfidence > bestConfidence) {
+	            bestConfidence = currentConfidence;
+	            predicate_label = currentPrediction;
+	        }
+	    }
+		
+		
+		return predicate_label;
 	}
+	
+	private Hashtable<String, Double> create(String s) {
+        Hashtable<String, Double> result = new Hashtable<String, Double>();
+        result.put(s, 1.0);
+        return result;
+    }
 }
